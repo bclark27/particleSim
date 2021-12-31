d9c11ba85c93e34f087dfcef691c14223db92f94
Ben <benjamin.surfer14@gmail.com>
2021-12-31 13:59:15 -0500

    first commit

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PUSH_INTERFACE 1
+#define PUSH_PROD_ID 21
+#define PUSH_VEND_ID 2536
+#define msleep(x) usleep(x * 1000)
+
+////////////////////
+//  PRIVATE VARS  //
+////////////////////
+
+int main()
+{
+    
+}
